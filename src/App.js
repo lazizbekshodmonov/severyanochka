59a1eefa666230b77151eda290f418cae65912b2
Lazizbek Shodmonov <lazizbekshodmonov001@gmail.com>
2022-05-26 13:53:52 +0500

    New project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">
+                <span>Icon</span>
+                <p>Избранное</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                <span>Icon</span>
+                <p>Избранное</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/users">
+                <span>Icon</span>
+                <p>Избранное</p>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/about">
+           
+          </Route>
+          <Route path="/users">
+            
+          </Route>
+          <Route path="/">
+            
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
